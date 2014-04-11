9934d4d284d27a77602f58ed5b125ea0d2e34a8e
vinco <zappeln@gmail.com>
2014-04-10 20:34:48 -0500

    finishing up catstie

--- tic_tac_toe/tictaccomputer.rb
+++ tic_tac_toe/tictaccomputer.rb
@@ -55,23 +55,23 @@ class Computer
     when 8
       @rolls << 0
     when 0
-      if @rolls[4] == "   "
+      if @grid[4] == "   "
         @rolls << 4
-      elsif @rolls[2] == "   "
+      elsif @grid[2] == "   "
         @rolls << 2
-      elsif @rolls[6] == "   "
+      elsif @grid[6] == "   "
         @rolls << 6
       end
     when 2
-      if @rolls[1] == "   "
+      if @grid[1] == "   "
         @rolls << 1
-      elsif @rolls[5] == "   "
+      elsif @grid[5] == "   "
         @rolls << 5
       end
     when 6
-      if @rolls[3] == "   "
+      if @grid[3] == "   "
         @rolls << 3
-      elsif @rolls[7] == "   "
+      elsif @grid[7] == "   "
         @rolls << 7
       end
     end 
@@ -82,14 +82,14 @@ class Computer
     when 8
       @rolls << 2
     when 2
-      if @rolls[5] == "   "
+      if @grid[5] == "   "
         @rolls << 5
       else
         @rolls << 6
     when 6
-      if @roll[4] == "   "
+      if @rgrid[4] == "   "
         @rolls << 4
-      elsif @rolls[7] == "   "
+      elsif @grid[7] == "   "
         @rolls << 7
       end
     end 
@@ -100,19 +100,56 @@ class Computer
     when 8
       @rolls << 4
     when 4
-      if @rolls[0] == "   "
+      if @grid[0] == "   "
         @rolls << 0
-      elsif @rolls[1] == " O "
+      elsif @grid[1] == " O "
         @rolls << 2
-      elsif @rolls[3] == " O "
+      elsif @grid[3] == " O "
         @rolls << 6
       end
     when 2
-
+      if @grid[5] == "   "
+        @rolls << 5
+      elsif @grid[6] == "   "
+        @rolls << 6
+      end
     when 6
-
+      if @grid[7] == "   "
+        @rolls << 7
+      elsif @grid[2] == "   "
+        @rolls << 2
+      end
     end 
   end
+
+  def catstie
+    case @rolls.last
+    when 8
+      @rolls << 0
+    when 0
+      if @grid[6] == " O "
+        @rolls << 2
+      elsif @grid[2] == " O "
+        @rolls << 6
+      elsif @grid[1] == " O "
+        @rolls << 7
+      elsif @grid[5] == " O "
+        @rolls << 3
+      elsif @grid[3] == " O "
+        @rolls << 5
+      elsif @grid[7] == " O "
+        @rolls << 1        
+      end
+    when 1 || 5
+      if @grid[2] == "   "
+        @rolls << 2
+      end
+    when 3 || 7
+      if @grid[6] == "   "
+        @rolls << 6
+      end
+    end
+  end       
 
 
   def pull_strategy
