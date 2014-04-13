e27c88043b047c38a48c8d489c3f2db497cbeed1
vinco <zappeln@gmail.com>
2014-04-12 19:15:04 -0500

    finished 1k fibonacci

---
+++ euler/25_1k_fib.rb
@@ -0,0 +1,32 @@
+
+# The Fi1bonacci sequence is defined by the recurrence relation:
+
+# Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+# Hence the first 12 terms will be:
+
+# F1 = 1
+# F2 = 1
+# F3 = 2
+# F4 = 3
+# F5 = 5
+# F6 = 8
+# F7 = 13
+# F8 = 21
+# F9 = 34
+# F10 = 55
+# F11 = 89
+# F12 = 144
+# The 12th term, F12, is the first term to contain three digits.
+
+# What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+
+fib_chain = [1, 1]
+
+while fib_chain.last.to_s.length < 1000
+  fib_chain << fib_chain[-1] + fib_chain[-2]
+end
+
+p fib_chain.length
+
+
